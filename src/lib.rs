7504137bf1d0253cc36fcd601f8ec910724ef799
Joakim Frostegård <joakim.frostegard@gmail.com>
2021-10-21 17:00:20 +0200

    Feature-gate socket.rs and dependencies async-io and futures-lite

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 #[cfg(unix)]
 mod cmsg;
 mod proto;
+#[cfg(feature = "with-async-io")]
 mod socket;
 #[cfg(unix)]
 pub mod unix;
 
 pub use proto::{EcnCodepoint, RecvMeta, SocketType, Transmit, UdpCapabilities};
+#[cfg(feature = "with-async-io")]
 pub use socket::UdpSocket;
 
 /// Number of UDP packets to send/receive at a time when using sendmmsg/recvmmsg.
